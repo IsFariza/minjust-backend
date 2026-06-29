8693a28c20db11823ae1c115eeb9d79e22b3b223
IsFariza <fariza.ismag@gmail.com>
2026-06-29 09:59:05 +0500

    polished README documentation

--- migrations/000001_create_admin.up.sql
+++ migrations/000001_create_admin.up.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS admin_accounts (
 );
 
 INSERT INTO admin_accounts (username, password_hash) 
-VALUES ('admin', '$2a$10$1GNjZzXsGMxCgu6FEwvTsuaz6VKmZeT9Hc10h4QRI3c/D08hoHy0W')
+VALUES ('admin', '$2a$10$1GNjZzXsGMxCgu6FEwvTsuaz6VKmZeT9Hc10h4QRI3c/D08hoHy0W') 
 ON CONFLICT (username) DO NOTHING;
